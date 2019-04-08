2b04260bfcb21ab4b0a5e936e4e336b00654cdd7
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:53:41 +0000

    Mon Apr 08 13:53:41 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -4,41 +4,31 @@ import numpy as np
 def fizz_buzz(number):
     digits = [int(d) for d in str(number)]
     deluxe = (number % 3 == 0 and 3 in digits) or (number % 5 == 0 and 5 in digits)
+    fake = (number % 2 != 0)
+    fizz = (number % 3 == 0 or 3 in digits)
+    buzz = (number % 5 == 0 or 5 in digits)
 
-    if (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits) and deluxe and (number % 2 != 0):
+    if fizz and buzz and deluxe and fake:
         return "fizz buzz fake deluxe"
-    elif (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits) and deluxe:
+    elif fizz and buzz and deluxe:
         return "fizz buzz deluxe"
-    elif (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits):
+    elif fizz and buzz:
         return "fizz buzz"
-    elif (number % 5 == 0 or 5 in digits) and deluxe and (number % 2 != 0):
+    elif buzz and deluxe and fake:
         return "buzz fake deluxe"
-    elif (number % 5 == 0 or 5 in digits) and deluxe:
+    elif buzz and deluxe:
         return "buzz deluxe"
-    elif number % 5 == 0 or 5 in digits:
+    elif buzz:
         return "buzz"
-    elif (number % 3 == 0 or 3 in digits) and deluxe and (number % 2 != 0):
+    elif fizz and deluxe and fake:
         return "fizz fake deluxe"
-    elif (number % 3 == 0 or 3 in digits) and deluxe:
+    elif fizz and deluxe:
         return "fizz deluxe"
-    elif number % 3 == 0 or 3 in digits:
+    elif fizz:
         return "fizz"
-    elif deluxe and (number % 2 != 0):
+    elif deluxe and fake:
         return "fake deluxe"
     elif deluxe:
         return "deluxe"
     else:
         return number
-
-
-
-
-
-
-# Remove deluxe
-# put conditions in list/dict?
-# should definitely do that!
-                                        # deluxe = condition
-                                        # fizz = condition
-                                        #if fizz and deluxe:
-                                        # return "fizz deluxe"
