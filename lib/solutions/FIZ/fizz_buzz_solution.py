713d233f9ffc0c6978d41517461f0b2f336d3bc6
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:12:36 +0000

    Mon Apr 08 13:12:36 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,4 +1,6 @@
 # noinspection PyUnusedLocal
+import numpy as np
+
 def fizz_buzz(number):
     digits = [int(d) for d in str(number)]
     if (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits):
@@ -7,5 +9,8 @@ def fizz_buzz(number):
         return "buzz"
     elif number % 3 == 0 or 3 in digits:
         return "fizz"
+    elif len(np.unique(digits)) == 1:
+        return "deluxe"
     else:
         return number
+
