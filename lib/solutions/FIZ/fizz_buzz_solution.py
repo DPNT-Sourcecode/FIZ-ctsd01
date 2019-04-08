5c33e3a85d76ab6724a311f3600ae4916f60f69b
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:20:07 +0000

    Mon Apr 08 13:20:07 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,22 +3,23 @@ import numpy as np
 
 def fizz_buzz(number):
     digits = [int(d) for d in str(number)]
-    if (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits) and ((len(np.unique(digits)) == 1) and len(digits) > 1):
+    if (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits) and ((len(np.unique(digits)) == 1) and number > 10):
         return "fizz buzz deluxe"
     elif (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits):
         return "fizz buzz"
-    elif number % 5 == 0 or 5 in digits and ((len(np.unique(digits)) == 1) and len(digits) > 1):
+    elif (number % 5 == 0 or 5 in digits) and ((len(np.unique(digits)) == 1) and number > 10):
         return "buzz deluxe"
     elif number % 5 == 0 or 5 in digits:
         return "buzz"
-    elif number % 3 == 0 or 3 in digits and ((len(np.unique(digits)) == 1) and len(digits) > 1):
+    elif (number % 3 == 0 or 3 in digits) and ((len(np.unique(digits)) == 1) and number > 10):
         return "fizz deluxe"
     elif number % 3 == 0 or 3 in digits:
         return "fizz"
-    elif len(np.unique(digits)) == 1 and len(digits) > 1:
+    elif len(np.unique(digits)) == 1 and number > 10:
         return "deluxe"
     else:
         return number
+
 
 
 
