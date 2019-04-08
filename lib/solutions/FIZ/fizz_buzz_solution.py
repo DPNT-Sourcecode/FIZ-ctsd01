b7cca41431640c7d66353114e6c6fc1ea44b5e41
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:38:03 +0000

    Mon Apr 08 13:38:03 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,32 +3,32 @@ import numpy as np
 
 def fizz_buzz(number):
     digits = [int(d) for d in str(number)]
-    if (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits) and ((len(np.unique(digits)) == 1) and number > 10) and (number % 2 != 0):
+    deluxe = (number % 3 == 0 and 3 in digits) or (number % 5 == 0 and 5 in digits)
+
+    if (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits) and deluxe and (number % 2 != 0):
         return "fizz buzz fake deluxe"
-    elif (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits) and ((len(np.unique(digits)) == 1) and number > 10):
+    elif (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits) and deluxe:
         return "fizz buzz deluxe"
     elif (number % 3 == 0 or 3 in digits) and (number % 5 == 0 or 5 in digits):
         return "fizz buzz"
-    elif (number % 5 == 0 or 5 in digits) and ((len(np.unique(digits)) == 1) and number > 10) and (number % 2 != 0):
+    elif (number % 5 == 0 or 5 in digits) and deluxe and (number % 2 != 0):
         return "buzz fake deluxe"
-    elif (number % 5 == 0 or 5 in digits) and ((len(np.unique(digits)) == 1) and number > 10):
+    elif (number % 5 == 0 or 5 in digits) and deluxe:
         return "buzz deluxe"
     elif number % 5 == 0 or 5 in digits:
         return "buzz"
-    elif (number % 3 == 0 or 3 in digits) and ((len(np.unique(digits)) == 1) and number > 10) and (number % 2 != 0):
+    elif (number % 3 == 0 or 3 in digits) and deluxe and (number % 2 != 0):
         return "fizz fake deluxe"
-    elif (number % 3 == 0 or 3 in digits) and ((len(np.unique(digits)) == 1) and number > 10):
+    elif (number % 3 == 0 or 3 in digits) and deluxe:
         return "fizz deluxe"
     elif number % 3 == 0 or 3 in digits:
         return "fizz"
-    elif len(np.unique(digits)) == 1 and number > 10 and (number % 2 != 0):
+    elif deluxe and (number % 2 != 0):
         return "fake deluxe"
-    elif len(np.unique(digits)) == 1 and number > 10:
+    elif deluxe:
         return "deluxe"
     else:
         return number
-
-
 
 
 
@@ -42,6 +42,7 @@ def fizz_buzz(number):
                                         # fizz = condition
                                         #if fizz and deluxe:
                                         # return "fizz deluxe"
+
 
 
 
